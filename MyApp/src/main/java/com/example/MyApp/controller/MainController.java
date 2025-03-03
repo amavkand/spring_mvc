734b10e0f537d748db7621d0187fdc4707a3d3e6
Аверин Максим <maks22rus120303@mail.ru>
2025-03-03 21:52:27 +0700

    Пятый урок

--- MyApp/src/main/java/com/example/MyApp/controller/MainController.java
+++ MyApp/src/main/java/com/example/MyApp/controller/MainController.java
@@ -26,10 +26,19 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(
+            @RequestParam(required = false, defaultValue = "") String filter,
             Model model
     ) {
         Iterable<Message> messages = messageRepository.findAll();
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
         model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -41,18 +50,7 @@ public class MainController {
         }
         Iterable<Message> messages = messageRepository.findAll();
         model.addAttribute("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Model model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.addAttribute("messages", messages);
+        model.addAttribute("filter", "");
         return "main";
     }
 }
